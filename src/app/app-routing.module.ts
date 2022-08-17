d92df14ddf2e12aa5c8726eff82800656ce0bdc0
felipeamorim-dev <felipeamorim.dev@gmail.com>
2022-08-16 23:30:02 -0300

    :sparkles: adicionado modulo de login para o componente de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./components/apresentacao/apresentacao.module').then(m => m.ApresentacaoModule) }
+  { path: '', loadChildren: () => import('./components/apresentacao/apresentacao.module').then(m => m.ApresentacaoModule) },
+  { path: 'login', loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule) }
 ];
 
 @NgModule({
