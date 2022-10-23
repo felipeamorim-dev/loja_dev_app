c3f053baf6e7bdee8fcfcdac8d21d2d5db7dfa40
Felipe Amorim <felipeamorim.dev@gmail.com>
2022-10-22 22:57:33 -0300

    refactor: modificado a tela de cadastro do usuário

--- src/app/components/login/cadastro/cadastro.component.ts
+++ src/app/components/login/cadastro/cadastro.component.ts
@@ -1,4 +1,7 @@
+import { Usuario } from './../../../model/usuario';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from '../../shared/dialog/dialog.component';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario = {
+    nome: 'Felipe da Silva Amorim',
+    nomeUsuario: 'felipeam-dev',
+    email: 'felipeam.dev@gmail.com',
+    senha: '123',
+    urlImagePerfil: '',
+    perfis: [
+      "CLIENTE"
+    ]
+  }
+
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+      data: this.usuario
+    })
   }
 
 }
