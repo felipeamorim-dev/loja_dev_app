2354da3695e27570a73c7607ba26e727dfe039a9
Felipe Amorim <felipeamorim.dev@gmail.com>
2022-10-27 01:32:43 -0300

    feat: adição do guarda de rota para o componente home

--- src/app/components/home/home-routing.module.ts
+++ src/app/components/home/home-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GuardGuard } from 'src/app/core/guard.guard';
 import { HomeComponent } from './home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', canActivate: [GuardGuard], component: HomeComponent }
 ];
 
 @NgModule({
