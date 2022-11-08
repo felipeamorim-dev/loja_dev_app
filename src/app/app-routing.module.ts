176d19f6935eeb7ae1013a7c7fbd9dc0775d5dbe
Felipe Amorim <felipeamorim.dev@gmail.com>
2022-11-07 23:31:18 -0300

    feat: adicionando as rotas dos componentes carrinho e página de error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: () => import('./components/apresentacao/apresentacao.module').then(m => m.ApresentacaoModule) },
   { path: 'login', loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule) },
-  { path: 'home', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule) }
+  { path: 'home', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule) },
+  { path: 'carrinho', loadChildren: () => import('./components/carrinho/carrinho.module').then(m => m.CarrinhoModule) },
+  { path: '404', loadChildren: () => import('./components/pag-error/pag-error.module').then(m => m.PagErrorModule) },
+  { path: '**', redirectTo: '404'}
 ];
 
 @NgModule({
